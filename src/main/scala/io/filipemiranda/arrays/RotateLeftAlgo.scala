c0fbdb43f10bd820414df6f62c58a5211bda42aa
Filipe Miranda <felipe_getup@hotmail.com>
2021-04-21 23:46:47 +0200

    Add new algo to solve array rotation problem

--- src/main/scala/io/filipemiranda/arrays/RotateLeftAlgo.scala
+++ src/main/scala/io/filipemiranda/arrays/RotateLeftAlgo.scala
@@ -4,7 +4,7 @@ object RotateLeftAlgo {
   /*
     A left rotation operation on an array shifts each of the array's elements  unit to the left. For example, if  left rotations are performed on array , then the array would become . Note that the lowest index item moves to the highest index in a rotation. This is called a circular array.
     Given an array  of  integers and a number, , perform  left rotations on the array. Return the updated array to be printed as a single line of space-separated integers.
-     */
+     */ //`TODO: question is this left or right rotation??? xD
   def rotLeftBruteForce(array: Array[Int], nrOfMoves: Int): Array[Int] = {
     for (_ <- 1 to nrOfMoves) {
       for (i <- array.indices) {
@@ -16,6 +16,18 @@ object RotateLeftAlgo {
       }
     }
     array
+  }
+
+  /*
+   * Good algo resolves the problem in O(n) with an extra space of O(N)
+  */
+  def rotateLeft(simpleArray: Array[Int], nrOfMoves: Int): Array[Int] = {
+    val resultingArray = new Array[Int](simpleArray.length)
+    for (i <- simpleArray.indices) {
+      val newIndex = (i + (simpleArray.length - nrOfMoves)) % simpleArray.length
+      resultingArray(newIndex) = simpleArray(i)
+    }
+    resultingArray
   }
 
 }
--- src/test/scala/io/filipemiranda/arrays/RotateLeftAlgoTest.scala
+++ src/test/scala/io/filipemiranda/arrays/RotateLeftAlgoTest.scala
@@ -12,11 +12,29 @@ class RotateLeftAlgoTest extends AnyFlatSpec {
     result shouldEqual(expectedSimpleArrayRotated)
   }
 
-  "Brute force rotLeft function" should "left rotate the array items to the left the given numberOfTimes -> 3 time " in {
+  "2 - Brute force rotLeft function" should "left rotate the array items to the left the given numberOfTimes -> 3 time " in {
     val simpleArray = Array(1,2,3,4,5)
     val expectedSimpleArrayRotated = Array(4,5,1,2,3)
     val result = RotateLeftAlgo.rotLeftBruteForce(simpleArray, 3)
     result shouldEqual(expectedSimpleArrayRotated)
   }
+
+  "A good implementation rotate left, " should " rotate left the array in linear time -> 3 times" in {
+    val simpleArray = Array(1,2,3,4,5)
+    val expectedSimpleArrayRotated = Array(4,5,1,2,3)
+    val result = RotateLeftAlgo.rotateLeft(simpleArray, 3)
+    result shouldEqual(expectedSimpleArrayRotated)
+  }
+
+  "A good implementation rotate left, " should " rotate left the array in linear time -> 1 time" in {
+    val simpleArray = Array(1,2,3,4,5)
+    val expectedSimpleArrayRotated = Array(2,3,4,5,1)
+    val result = RotateLeftAlgo.rotateLeft(simpleArray, 1)
+    result shouldEqual(expectedSimpleArrayRotated)
+  }
+
+
+
+
 
 }
